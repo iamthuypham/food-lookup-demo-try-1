a419f8cf3405662ee4ec18fd597284c382eaa8be
Thuy <tpham045@hotmail.com>
2017-01-19 23:19:16 +0000

    Migrating

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import './semantic-ui/semantic.min.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ClientApp from './ClientApp'
 
 ReactDOM.render(
-  <App />,
+  <ClientApp />,
   document.getElementById('root')
-);
+)
